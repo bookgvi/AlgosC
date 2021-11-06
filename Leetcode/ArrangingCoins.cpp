150b6a1a44f129978da0402875b916767ee57884
bookgvi <bookgvi@gmail.com>
2021-11-06 10:57:46 +0300

    leetcode task arrangincoins

---
+++ Leetcode/ArrangingCoins.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        vector<int> dp;
+        dp.push_back(1);
+        n -= dp[0];
+        int k = 1;
+        while (n > dp[dp.size() - 1]) {
+            dp.push_back(dp[k - 1] + 1);
+            n -= dp[k];
+            k += 1;
+        }
+        return dp.size();
+    }
+};
+
+int arrangin_coins_main() {
+    Solution s;
+    int res = s.arrangeCoins((1 << 31) - 1);
+}
